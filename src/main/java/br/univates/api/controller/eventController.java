2d870ab0d8cba40dbd982e471d419b11e611306b
Lucas Gustavo Weber <lucas.weber1@universo.univates.br>
2025-10-16 14:56:08 -0300

    Liberado acesso do frontend

--- src/main/java/br/univates/api/controller/eventController.java
+++ src/main/java/br/univates/api/controller/eventController.java
@@ -4,13 +4,11 @@ import br.univates.api.model.Events;
 import br.univates.api.repositories.eventRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/events")
 public class eventController {
